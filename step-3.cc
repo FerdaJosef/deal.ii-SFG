62b928059e1496dfa75a65edb78fba53b678b1f8
FerdaJosef <josef.ferda@hotmail.cz>
2026-03-15 15:56:54 +0100

    After discussion with Karel and Jirka

--- step-3.cc
+++ step-3.cc
@@ -47,8 +47,62 @@
 #include "equation.h"
 
 #include <math.h>
+#include <deal.II/base/conditional_ostream.h>
+#include <deal.II/lac/affine_constraints.h>
+
+#include <deal.II/grid/grid_tools.h>
+ 
+#include <deal.II/dofs/dof_renumbering.h>
+ 
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/fe/mapping_q.h>
+ 
+#include <deal.II/numerics/vector_tools.h>
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/error_estimator.h>
+#include <deal.II/lac/solver_gmres.h>
 
 using namespace dealii;
+
+class RightHandSide : public Function<3>
+{
+public:
+  RightHandSide(double time = 0.0)
+    : Function(2, time)
+    , period(0.2)
+  {}
+
+  virtual double value(const Point<3>  &p,
+                       const unsigned int component) const override;
+  
+  private:
+    const double period;
+};
+
+
+
+double RightHandSide::value(const Point<3> &p,
+                                 const unsigned int component) const
+{
+    const double t = this->get_time();
+
+    std::random_device rd{};
+    std::mt19937 gen{rd()};
+    std::normal_distribution<double> d{0.5, 0.05};
+
+    double sample;
+    sample = d(gen);
+
+    if (component == 0)
+      return std::sin(M_PI*p[0])*std::sin(M_PI*p[1])*std::sin(M_PI*p[2]);
+
+    else if (component == 1)
+      return std::sin(M_PI*p[0])*std::sin(M_PI*p[1])*std::sin(M_PI*p[2]);
+
+  else
+    return 0.0;
+
+}
 
 class BoundaryValues : public Function<3>
 {
@@ -68,29 +122,26 @@ double BoundaryValues::value(const Point<3> &p,
 {
   return 0.0;
 }
-
+/*
 class ExactSolution : public Function<3>
 {
   public:
-  ExactSolution()
-  : Function<3>(2)
+  ExactSolution(double time = 0.0)
+  : Function<3>(2, time)
   {}
 
   virtual double value(const Point<3> &p,
                       const unsigned int component) const override
   {
+    const double t = this->get_time();
 
-    //std::random_device rd{};
-    //std::mt19937 gen{rd()};
-    //std::normal_distribution<double> d{0.5, 0.05};
+    std::random_device rd{};
+    std::mt19937 gen{rd()};
+    std::normal_distribution<double> d{0.5, 0.05};
 
-    //double sample;
-    //sample = d(gen);
-    if (component == 0)
-      return std::sin(M_PI*p[0]) * std::sin(M_PI*p[1]);
-    
-    else
-      return std::cos(M_PI*p[0]) * std::sin(M_PI*p[1]);
+    double sample;
+    sample = d(gen);
+    return sample;
 
   }
 };
@@ -117,7 +168,7 @@ void right_hand_side(const std::vector<Point<3>> &points,
         + u;
   }
 }
-
+*/
 class Step3
 {
 public:
@@ -131,7 +182,10 @@ private:
   void setup_system();
   void assemble_system();
   void solve();
-  void output_results(const unsigned int current_refinement_cycle) const;
+  void time_step_update();
+  double determine_step_length() const;
+  void output_results() const;
+  void generate_rhs(const unsigned int x_q_ps);
   double compute_residual();
 
   Triangulation<3> triangulation;
@@ -149,21 +203,58 @@ private:
   Vector<double> solution;
   Vector<double> system_rhs;
 
+  std::vector<Tensor<1,2>> rhs_values;
+
+  double time;
+  double final_time;
+  double delta_t;
+  unsigned int timestep_number;
+
+  int max_it;
+  double max_multiplier;
+  double min_multiplier;
+  int optimal_it;
+  double dt_max; double dt_min;
+  int newton_iteration;
+
 };
 
 
 Step3::Step3()
   : fe(FE_Q<3>(1), 2)
   , dof_handler(triangulation)
+  , time(0.0)
+  , final_time(0.01)
+  , delta_t(0.001)
+  , timestep_number(0)
+  , max_it(10)
+  , max_multiplier(1.6)
+  , min_multiplier(0.5)
+  , optimal_it(6)
+  , dt_max(0.2)
+  , dt_min(1e-6)
+  , newton_iteration(0)
 {}
 
+void Step3::generate_rhs(const unsigned int n_q_points)
+{
+  rhs_values.resize(n_q_points);
 
+  static std::mt19937 gen(std::random_device{}());
+  static std::normal_distribution<double> dist(0.0, 1.0);
+
+  for (unsigned int q = 0; q < n_q_points; ++q)
+  {
+    rhs_values[q][0] = 1e-6*dist(gen);
+    rhs_values[q][1] = 1e-6*dist(gen);
+  }
+}
 
 void Step3::make_grid()
 {
-  GridGenerator::hyper_cube(triangulation, -1, 1);
+  GridGenerator::hyper_cube(triangulation, -10, 10, true);
   triangulation.refine_global(
-    4
+    3
   );
 
   std::cout << "Number of active cells: " << triangulation.n_active_cells()
@@ -180,19 +271,19 @@ void Step3::setup_system()
   VectorTools::interpolate_boundary_values(dof_handler,
                                             0,
                                             Functions::ZeroFunction<3>(2),
-                                            zero_constraints);
+                                            nonzero_constraints);
   zero_constraints.close();
 
   nonzero_constraints.clear();
-  VectorTools::interpolate_boundary_values(dof_handler,
-                                            0,
-                                            ExactSolution(),
-                                            nonzero_constraints);
+
+  DoFTools::make_periodicity_constraints(dof_handler, 0, 1, 0, nonzero_constraints); // x-direction
+  DoFTools::make_periodicity_constraints(dof_handler, 2, 3, 1, nonzero_constraints); // y-direction
+  DoFTools::make_periodicity_constraints(dof_handler, 4, 5, 2, nonzero_constraints); // z-direction
 
   nonzero_constraints.close();
 
   DynamicSparsityPattern dsp(dof_handler.n_dofs());
-  DoFTools::make_sparsity_pattern(dof_handler, dsp, zero_constraints, false);
+  DoFTools::make_sparsity_pattern(dof_handler, dsp, nonzero_constraints, true);
   sparsity_pattern.copy_from(dsp);
 
   system_matrix.reinit(sparsity_pattern);
@@ -224,6 +315,8 @@ void Step3::assemble_system()
 
   std::vector<types::global_dof_index> local_dof_indices(dofs_per_cell);
 
+  RightHandSide rhs_function;
+  rhs_function.set_time(this->time);
 
  const unsigned int dim=3;
   //======= ACEGEN input
@@ -260,7 +353,6 @@ void Step3::assemble_system()
           std::vector<Tensor<2,dim>>(2)));
   //======= ACEGEN=======
 
-  std::vector<Tensor<1, 2>> rhs_values(n_q_points);
 
 
   for (const auto &cell : dof_handler.active_cell_iterators())
@@ -274,18 +366,22 @@ void Step3::assemble_system()
       fe_values.get_function_values(oldsolution, values_old);
       fe_values.get_function_values(solution, values_newton);
 
-      right_hand_side(fe_values.get_quadrature_points(), rhs_values);
+      //right_hand_side(fe_values.get_quadrature_points(), rhs_values);
 
       for (const unsigned int q_index : fe_values.quadrature_point_indices())
         {
+        const auto &x_q = fe_values.quadrature_point(q_index);
+
         equation(acegen_scratch,
                 values_newton[q_index],
+                values_old[q_index],
             gradients_newton[q_index],
             dPsiDu[q_index],
             dPsidGradU[q_index],
             dPsiDu2[q_index],
             dPsidUdGradU[q_index],
-            dPsidGradU2[q_index] // 2, 2, 3, 3 
+            dPsidGradU2[q_index], // 2, 2, 3, 3
+            &this->delta_t 
           );
 
           for (const unsigned int i : fe_values.dof_indices())
@@ -303,7 +399,7 @@ void Step3::assemble_system()
                               dPsiDu[q_index][component_i]
 
                           + fe_values.shape_grad(i, q_index) * dPsidGradU[q_index][component_i]
-                            - fe_values.shape_value(i,q_index) * rhs_values[q_index][component_i]) *
+                             - fe_values.shape_value(i,q_index) * rhs_values[q_index][component_i]) *
                               fe_values.JxW(q_index);
               }
           for (const unsigned int i : fe_values.dof_indices())
@@ -342,25 +438,14 @@ void Step3::assemble_system()
     }
 }
 
-double Step3::compute_residual()
+double Step3::determine_step_length() const
 {
-  Vector<float> difference_per_cell(triangulation.n_active_cells());
-  VectorTools::integrate_difference(dof_handler,
-                                    solution,
-                                    ExactSolution(),
-                                    difference_per_cell,
-                                    QGauss<3>(fe.degree + 1),
-                                    VectorTools::L2_norm);
-  const double L2_error =
-  VectorTools::compute_global_error(triangulation,
-                                    difference_per_cell,
-                                    VectorTools::L2_norm);
-  return L2_error;
+  return 1.0;
 }
 
 void Step3::solve()
 {
-  SolverControl            solver_control(5000, 1e-6 * system_rhs.l2_norm());
+  SolverControl            solver_control(5000, 1e-9 * system_rhs.l2_norm());
   SolverGMRES<Vector<double>> solver(solver_control);
 
   PreconditionJacobi<SparseMatrix<double>> preconditioner;
@@ -370,26 +455,56 @@ void Step3::solve()
 
   zero_constraints.distribute(newton_iterate);
 
-  solution.add(1.0, newton_iterate);
+  const double alpha = determine_step_length();
+
+  solution.add(alpha, newton_iterate);
+
+  nonzero_constraints.distribute(solution);
 
   std::cout << solver_control.last_step()
             << " iterations needed to obtain convergence." << std::endl;
 }
 
-
-
-void Step3::output_results(const unsigned int refinement_cycle) const
+void Step3::time_step_update()
 {
+    if (newton_iteration <= optimal_it) {
+      // self.dt.assign(min(dt_max, float(self.dt) * min(1.6, optimal_it / (iterations + 0.001))));
+        delta_t = std::min(dt_max, delta_t*std::min(max_multiplier, double(optimal_it/(newton_iteration + 0.001))));
+    }
+    else {
+      // self.dt.assign(float(self.dt) * max(0.5, optimal_it / iterations));
+      delta_t = delta_t*std::max(min_multiplier, double(optimal_it / newton_iteration));
+    }
+
+    if (time + delta_t > final_time) {
+        delta_t = final_time - time;
+    }
+      
+    else if (delta_t < 1e-6) {
+        throw std::invalid_argument("Time step too small!");
+    }
+    std::cout << "Our time step is " << delta_t << std::endl;
+}
+
+void Step3::output_results() const
+{
+  static std::vector<std::pair<double, std::string>> times_and_names;
+
   DataOut<3> data_out;
   data_out.attach_dof_handler(dof_handler);
   data_out.add_data_vector(solution, "solution");
   data_out.build_patches();
 
-  const std::string filename =
-        "solution-" + Utilities::int_to_string(refinement_cycle, 2) + ".vtk";
-  std::ofstream     output(filename);
-  data_out.write_vtk(output);
+  const std::string filename = "output/solution-" + Utilities::int_to_string(timestep_number) + ".vtu";
+  std::ofstream output(filename);
+  data_out.write(output, DataOutBase::vtu);
+
   std::cout << "Output written to " << filename << std::endl;
+  times_and_names.push_back(
+                {time, filename});
+
+  std::ofstream pvd_output ("solution.pvd");
+  DataOutBase::write_pvd_record(pvd_output, times_and_names);
 }
 
 
@@ -399,35 +514,59 @@ void Step3::run()
   make_grid();
   setup_system();
 
-  VectorTools::project(dof_handler,
-                        nonzero_constraints,
-                        QGauss<3>(fe.degree + 1),
-                        ExactSolution(),
-                        solution);
+  solution = 0.0;
 
   nonzero_constraints.distribute(solution);
-  output_results(3);
 
-  double residual_norm = 1.0;
+  //std::cout << "The difference is " << L2_error << std::endl;
 
-  int newton_iteration = 0;
+  output_results();
 
-  while (newton_iteration < 10 && residual_norm > 1e-10)
+  std::cout << time << std::endl;
+  while (time < final_time)
   {
-    assemble_system();
-    residual_norm = system_rhs.l2_norm();
+    time+=delta_t;
+    timestep_number++;
 
-    solve();
+    generate_rhs(QGauss<3>(fe.degree + 1).size());
 
-    std::cout << "  Residual: " << residual_norm << std::endl;
-    newton_iteration++;
+    std::cout<< "Time: " << time << std::endl;
+    oldsolution = solution;
+    newton_iteration = 0;
+    double residual_norm = 1.0;
+    while (residual_norm > 1e-10 && newton_iteration < max_it)
+    {
+      //newton_iterate = 0.0;
+      assemble_system();
+      residual_norm = system_rhs.l2_norm();
+
+      std::cout << "The norm of our solution is: " << residual_norm << std::endl;
+
+      solve();
+
+      //std::cout << "  Residual: " << residual_norm << std::endl;
+      newton_iteration++;
+    }
+
+    if (newton_iteration == max_it)
+    {
+        std::cout << "Newton failed. Reducing time step." << std::endl;
+        solution = oldsolution;
+
+        time -= delta_t;
+        timestep_number--;
+
+        delta_t *= 0.5;
+        if (delta_t < dt_min)
+            throw std::runtime_error("Time step too small!");
+
+        continue;
+    }
+
+    std::cout << newton_iteration << std::endl;
+    time_step_update();
+    output_results();
   }
-
-  double error = compute_residual();
-  std::cout << "Deviation is " << error << std::endl;
-
-  output_results(5);
-
 }
 
 
